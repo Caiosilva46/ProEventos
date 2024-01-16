e5002dd9955f9f41c8bba26cd8b9cc591fd15fa2
Caio Silva G <caio_italy@hotmail.com>
2024-01-16 16:33:44 -0300

    Primeiro commit da aplicação

---
+++ Backend/src/ProEvetos.API/Controllers/EventoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEvetos.API.Models;
+
+namespace ProEvetos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]
+            {
+                new Evento()
+                {
+                    EventoId = 1,
+                    Tema = "Angular + .NET",
+                    Local = "São paulo",
+                    Lote = "1ª Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemURL = "Foto.png"
+                },
+                new Evento()
+                {
+                    EventoId = 2,
+                    Tema = "Angular + .NET 6",
+                    Local = "São paulo",
+                    Lote = "2ª Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                    ImagemURL = "Foto.png"
+                },
+            };
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "POST";
+        }
+
+    }
+}
